b822e35ca48a8bb7f796df375eb6efea814fe77d
Neriya Cohen <51446652+neriya333@users.noreply.github.com>
2023-04-19 23:20:46 +0300

    Update Auto_Encoder_MNIST.py

--- ex2/Auto_Encoder_MNIST.py
+++ ex2/Auto_Encoder_MNIST.py
@@ -18,7 +18,7 @@ test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=F
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def train_autoencoder(encoder, decoder, save_model=False, d=10, layers=2):
+def train_autoencoder(encoder, decoder, d=10, layers=2):
     encoder = encoder.to(device)
     decoder = decoder.to(device)
 
@@ -126,24 +126,75 @@ class Decoder_depth2(nn.Module):
         return x
 
 
-def q1_1_train():
+class Encoder_depth(nn.Module):
+    def __init__(self, d, depth):
+        super(Encoder_depth, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, stride=2, padding=1)
+        self.convDepth = nn.Conv2d(32, 32, 3, stride=1, padding=1)
+        self.fc = nn.Linear(32 * 7 * 7, d)
+        self.flatten = nn.Flatten()
+        self.d = d
+        self.depth = depth
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        for i in range(self.depth):
+            x = torch.relu(self.convDepth(x))
+        x = self.flatten(x)
+        x = self.fc(x)
+        return x
+
+
+class Decoder_depth(nn.Module):
+    def __init__(self, d, depth):
+        super(Decoder_depth, self).__init__()
+        self.fc = nn.Linear(d, 32 * 7 * 7)
+        self.convDepth = nn.Conv2d(32, 32, 3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(128, 256, 3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+        self.conv1 = nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1)
+        self.conv2 = nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1)
+        
+        self.depth = depth
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.view(-1, 32, 7, 7)
+        for i in range(self.depth):
+            x = torch.relu(self.convDepth(x))
+        x = torch.relu(self.conv1(x))
+        x = torch.sigmoid(self.conv2(x))
+        return x
+
+
+def q1_train(type_of_autoencoder='different_d', parameters=None):
     """"q1 same architectures, different lateral space size"""
-    res = [train_autoencoder(Encoder_depth2(d), Decoder_depth2(d), False, d) for d in [4, 10, 16, 64, 128, 256]]
-    # res = [train_autoencoder(Encoder_depth2(d), Decoder_depth2(d), False, d) for d in [128]]
-    # res = ['b','f','r']
-    with open("train_results.csv", "w", newline="") as file:
+    if parameters is None:
+        parameters = [4, 10, 16, 64, 128, 256]
+    if type_of_autoencoder == 'different_d':
+        res = [train_autoencoder(Encoder_depth2(d), Decoder_depth2(d), d) for d in parameters]  # q1_1
+    else:
+        d = 10
+        res = [train_autoencoder(Encoder_depth(d, depth), Decoder_depth(d, depth), d) for depth in
+               parameters]  # q1_2
+    with open(f'train_results_{type_of_autoencoder}.csv', "w", newline="") as file:
         # Create a CSV writer object
         writer = csv.writer(file)
-        writer.writerow("pairs of training and test results")
         for train_test_res in res:
             writer.writerow(train_test_res[0])
             writer.writerow(train_test_res[1])
 
 
-def q1_plot():
+def q1_plot(type_of_autoencoder='different_d', parameters=None, plt_shape=(2, 3),
+            graph_title="loss on a 6-layers depth with latent space:"):
+    if parameters is None:
+        parameters = ['4', '10', '16', '64', '128', '256']
     training_loss = []
     test_loss = []
-    with open('train_results.csv', "r") as file:
+    with open(f'train_results_{type_of_autoencoder}.csv', "r") as file:
         reader = csv.reader(file)
         for i, line in enumerate(reader, 0):
             losses = list(map(float, line))
@@ -153,26 +204,27 @@ def q1_plot():
                 test_loss.append(losses)
 
     # Create subplots
-    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 8))
+    fig, axes = plt.subplots(nrows=plt_shape[0], ncols=plt_shape[1], figsize=(12, 8))
 
     # Flatten axes to a 1D array
     axes = axes.flatten()
-    title = ['4', '10', '16', '64', '128', '256']
+
+    plots_titles = parameters
     # Loop through the axes and plot the data
     for i in range(len(training_loss)):
         # Get the data for the subplot
         epochs = range(30)
-        label = 'training' if i % 2 == 0 else 'test'
+        # label = 'training' if i % 2 == 0 else 'test'
 
         # Plot the data
-        axes[i].plot(epochs[1:len(training_loss[i]) + 1], training_loss[i], label=label)
-        axes[i].plot(epochs[1:len(test_loss[i]) + 1], test_loss[i], label=label)
+        axes[i].plot(epochs[1:len(training_loss[i]) + 1], training_loss[i], label='train')
+        axes[i].plot(epochs[1:len(test_loss[i]) + 1], test_loss[i], label='test')
         if i == 0 or i == 3: axes[i].set_ylabel('loss')
         if i > 2: axes[i].set_xlabel('epochs')
-        axes[i].set_title(label=title[i])
+        axes[i].set_title(label=plots_titles[i])
         axes[i].legend()
 
-        plt.suptitle(t="loss on a 6-layers depth with latent space:")
+        plt.suptitle(t=graph_title)
 
     # Adjust spacing between subplots
     plt.subplots_adjust(wspace=0.4, hspace=0.4)
@@ -183,14 +235,15 @@ def q1_plot():
     input('finished')
 
 
-def q1_2_train():
-    pass
-
-
 def main():
-    # q1_1_train()
-    # q_1_plot()
-    q1_2_train()
+    # q1_train()
+    # q1_plot()
+
+    parameters = [1, 2, 3, 4, 5]
+    # q1_train('varying_depth', parameters) # after that I added to the output file 2 lines from the constant 2 depth layer trained in the first line of main
+    q1_plot(type_of_autoencoder='varying_depth', parameters=[str(x) for x in [0]+parameters], plt_shape=(2, 3),
+            graph_title='different net-depth, 6+2*')
+    input('here')
 
 
 # Press the green button in the gutter to run the script.
